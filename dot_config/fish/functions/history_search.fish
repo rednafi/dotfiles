2bbd6b0594c4807494c65c92556b32af7f26eaee
rednafi <redowan.delowar@wolt.com>
2025-10-11 19:59:29 +0200

    feat: add aerospace configuration

--- dot_config/fish/functions/history_search.fish
+++
@@ -1,7 +0,0 @@
-# Fuzzy history search with fzf
-function history_search
-    set -l selected (history | fzf --exact)
-    if test -n "$selected"
-        commandline --replace "$selected"
-    end
-end
